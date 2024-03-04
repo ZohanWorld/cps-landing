296c7220f2bec4031bfdf88eb374349d90e93656
Zohan World <zohan.developer@gmail.com>
2024-03-04 14:39:38 +0300

    Dev update
    v 1.0.1

--- src/blocks/aside-call/aside-call.js
+++ src/blocks/aside-call/aside-call.js
@@ -1,30 +1,21 @@
 const callButton = document.querySelectorAll('.button-repair');
 const asideMenu = document.querySelector('.aside-call');
 const closeCall = document.querySelector('.aside-call__close-button');
-<<<<<<< Updated upstream
-=======
 const mainContent = document.querySelector('main');
 const bodyContent = document.querySelector('body');
->>>>>>> Stashed changes
 
 const listener = function(button) {
  button.addEventListener('click', function(){
   asideMenu.classList.add('hide-call');
-<<<<<<< Updated upstream
-=======
   mainContent.classList.add('call-blur');
   bodyContent.style.overflow = 'hidden';
->>>>>>> Stashed changes
  });
 }
 
 closeCall.addEventListener('click', function(){
   asideMenu.classList.remove('hide-call');
-<<<<<<< Updated upstream
-=======
   mainContent.classList.remove('call-blur');
   bodyContent.style.overflow = 'auto';
->>>>>>> Stashed changes
 });
 
 for(let i = 0; i < callButton.length; i++) {
