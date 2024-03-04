a3fe8f4d296c0277dc26beae4bddad74207a5742
Zohan World <zohan.developer@gmail.com>
2024-03-04 14:05:10 +0300

    Dev update
    v 1.0.1

--- src/blocks/aside-menu/aside-menu.js
+++ src/blocks/aside-menu/aside-menu.js
@@ -1,11 +1,26 @@
 const burger = document.querySelector('.header-navigation__burger');
 const aside = document.querySelector('.aside-menu');
 const closeButton = document.querySelector('.aside-menu__button--close');
+<<<<<<< Updated upstream
 
 burger.addEventListener('click', function (){
   aside.classList.add('hide-aside');
+=======
+const mainContent = document.querySelector('main');
+const bodyContent = document.querySelector('body');
+
+burger.addEventListener('click', function (){
+  aside.classList.add('hide-aside');
+  mainContent.classList.add('call-blur');
+  bodyContent.style.overflow = 'hidden';
+>>>>>>> Stashed changes
 });
 
 closeButton.addEventListener('click', function (){
   aside.classList.remove('hide-aside');
+<<<<<<< Updated upstream
+=======
+  mainContent.classList.remove('call-blur');
+  bodyContent.style.overflow = 'auto';
+>>>>>>> Stashed changes
 });
--- src/blocks/aside-status/aside-status.js
+++ src/blocks/aside-status/aside-status.js
@@ -1,15 +1,30 @@
 const callButton = document.querySelectorAll('.button-checkstatus');
 const asideMenu = document.querySelector('.aside-status');
 const closeCall = document.querySelector('.aside-status__close-button');
+<<<<<<< Updated upstream
+=======
+const mainContent = document.querySelector('main');
+const bodyContent = document.querySelector('body');
+>>>>>>> Stashed changes
 
 const listener = function(button) {
  button.addEventListener('click', function(){
   asideMenu.classList.add('hide-status');
+<<<<<<< Updated upstream
+=======
+  mainContent.classList.add('status-blur');
+  bodyContent.style.overflow = 'hidden';
+>>>>>>> Stashed changes
  });
 }
 
 closeCall.addEventListener('click', function(){
   asideMenu.classList.remove('hide-status');
+<<<<<<< Updated upstream
+=======
+  mainContent.classList.remove('status-blur');
+  bodyContent.style.overflow = 'auto';
+>>>>>>> Stashed changes
 });
 
 for(let i = 0; i < callButton.length; i++) {
